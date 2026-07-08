fa06b89e14de879c5f23a6e5242c169ca5d078a1
adhamdeab45-cmyk <adhamdeab45@gmail.com>
2026-07-08 21:46:10 +0300

    Delete assets directory

--- assets/js/services/lottery.service.js
+++
@@ -1,3 +0,0 @@
-import { api } from "../core/api.js";
-export async function buyTicket(ticketNumber) { return api.buyTicket({ ticketNumber: String(ticketNumber || "").trim() }); }
-export function randomTicket() { return String(Math.floor(Math.random() * 1000000)).padStart(6, "0"); }
--- assets/js/services/wheel.service.js
+++
@@ -1,2 +0,0 @@
-import { api } from "../core/api.js";
-export async function spinWheel(bet) { return api.spinWheel({ bet: Number(bet) }); }

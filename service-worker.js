f437d521a9a4f6c8b92e68ec0e5dd1ac992e3917
adhamdeab45-cmyk <adhamdeab45@gmail.com>
2026-07-17 20:20:22 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='adnor-v208-auto-usdt-trc20-1';
+const CACHE='adnor-v209-global-badge-removed-1';
 const SHELL=[
   '/', '/index.html', '/app.html', '/offline.html', '/manifest.webmanifest',
   '/favicon.svg', '/apple-touch-icon.png', '/terms.html', '/privacy.html', '/responsible-play.html',

e51d8e380fbd0bbdc5002975d72a0bd80f35c680
adhamdeab45-cmyk <adhamdeab45@gmail.com>
2026-07-20 19:06:41 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='adnor-v225-withdraw-admin-state-sync-1';
+const CACHE='adnor-v226-referral-financial-reward-fix-1';
 const SHELL=[
   '/', '/index.html', '/app.html', '/offline.html', '/manifest.webmanifest',
   '/favicon.svg', '/apple-touch-icon.png', '/terms.html', '/privacy.html', '/responsible-play.html',
